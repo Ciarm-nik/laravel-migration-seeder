8e67b822819059b0e19ca9c231f8c8f739624510
Ciarm-nik <nikcia@libero.it>
2021-06-23 16:23:14 +0200

    Eseguito rollback e aggiunto nuova migrations

--- database/migrations/2021_06_23_140114_add_fields_to_packages_table.php
+++ database/migrations/2021_06_23_140114_add_fields_to_packages_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
 
 class AddFieldsToPackagesTable extends Migration
 {
@@ -15,6 +16,11 @@ class AddFieldsToPackagesTable extends Migration
     {
         Schema::table('packages', function (Blueprint $table) {
             $table-> string("descrizione_pacchetto", 255);
+            $table-> integer("prezzo_agenzia")->comment("il prezzo è obbligatorio");
+            $table-> date("partenza")->nullable();
+            $table-> date("ritorno")->nullable();
+            $table-> integer("sconto")->nullable();
+            $table-> integer("durata_viaggio")->nullable();
         });
     }
 
@@ -27,6 +33,11 @@ class AddFieldsToPackagesTable extends Migration
     {
         Schema::table('packages', function (Blueprint $table) {
             $table-> dropColumn("descrizione_pacchetto");
+            $table-> dropColumn("prezzo_agenzia");
+            $table-> dropColumn("partenza");
+            $table-> dropColumn("ritorno");
+            $table-> dropColumn("sconto");
+            $table-> dropColumn("durata_viaggio");
         });
     }
 }
